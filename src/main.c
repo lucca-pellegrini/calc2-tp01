b4d84d384ae659510cf8092faabf178e29b7b1d7
Lucca Pellegrini <luccapellegrini@e.email>
2024-09-24 19:31:00 -0300

    main.c: esboça exemplo de uso

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "func.h"
+#include "riemann.h"
+
 int main(void)
 {
-	puts("Hello, World!");
+	double coefs[] = { 0, 0, 2 };
+	Function *p = function_new(POLYNOMIAL, 2, coefs);
+
+	printf("%g\n", riemann(0, 1, p, 1000, ESQUERDA));
+
+	function_free(p);
 	return EXIT_SUCCESS;
 }
