086a67ecd057cb5b02afa3c8cd944acfa15644f2
Lucca Pellegrini <632054@sga.pucminas.br>
2024-09-25 15:28:42 -0300

    main.c: adiciona comentários explicando o exemplo

--- src/main.c
+++ src/main.c
@@ -6,11 +6,19 @@
 
 int main(void)
 {
-	double coefficients[] = { 0, 0, 2 };
-	Function *polynomial_ptr = function_new(POLYNOMIAL, 2, coefficients);
+	// Coeficientes para uma função polinomial:
+	// f(x) = 0x⁰ + 0x¹ + 2x² => f(x) = 2x²
+	double coeficientes[] = { 0, 0, 2 };
+	// Instancia um objeto Função de tipo Polinomial de grau 2
+	// (quadrático), usando os coeficientes acima.
+	Function *polinomio = function_new(POLYNOMIAL, 2, coeficientes);
 
-	printf("%g\n", riemann(0, 1, polynomial_ptr, 1 << 20, ESQUERDA));
+	// Avalia a soma de riemann no intervalo [0, 1] da função, usando
+	// 1,048,576 (2²⁰) retângulos, amostrando os valores de f(x_i) pela
+	// esquerda.
+	printf("%g\n", riemann(0, 1, polinomio, 1 << 20, ESQUERDA));
 
-	function_free(polynomial_ptr);
+	// Libera todos os recursos alocados para a função polinomial.
+	function_free(polinomio);
 	return EXIT_SUCCESS;
 }
